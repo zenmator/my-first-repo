0cce6b35c52d51dae29eddb9238dbee691b8ba1a
zenmator <leonardus.anwar@binus.ac.id>
2025-09-01 23:43:14 +0700

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Nama saya adalah Leonardus Narendra Banyu Anwar\n");
+    printf("Bahasa pemrograman favorit saya adalah Python \n");
+    return 0;
+}
+
